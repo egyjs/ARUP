afb9a791813b6c66a8325ca28e57bacb730eca91
Abdulrahman elzahaby <el3zahaby@gmail.com>
2019-06-28 00:39:41 +0300

    uploading a file: config.php

---
+++ up/posi-config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'gramoo');
+define('TIMEZONE', 'Asia/Kuwait');
+define('ENCRYPTION_KEY', '3ae709147204e435f201d0c3a4ba06ae');
